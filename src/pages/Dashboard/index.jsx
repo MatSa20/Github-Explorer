80467143403ac71048987c3251e74f4dacba7c83
matheus <matheus.sa.alves@outlook.com>
2020-09-01 18:29:25 -0300

    Fase 2: Comunicação e interação com a API pública do git

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,54 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Title, Form, Repositories } from './styeles'
+import api from '../../services/api'
 import { FiChevronRight } from 'react-icons/fi'
 import logoImg from '../../assets/logo.svg'
 
+
 export default function Dashboard() {
+  const [newRepo, setNewRepo] = useState('')
+
+  const [repositories, setRepositories] = useState([])
+
+  async function handleAddRepository(event) {
+    // Adicionar um repositorio
+    event.preventDefault()
+
+    const response = await api.get(`repos/${newRepo}`)
+
+    const repository = response.data
+
+    setRepositories([...repositories, repository])
+    setNewRepo('')
+  }
+
   return (
     <>
       <img src={logoImg} alt='Github Explorer' />
       <Title>Explore repositorios no Github</Title>
 
-      <Form action=''>
-        <input placeholder='Digite o nome do repositório' />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder='Digite o nome do repositório' />
         <button type='submit'>Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href='teste'>
-          <img src='https://avatars3.githubusercontent.com/u/38141771?s=460&u=671cdfd5dd82b545234d3ffeadf638bd286ddf9e&v=4'
-            alt='Matheus Sá' />
+        {repositories.map(repository => (
+          <a key={repository.full_name} href='teste'>
+            <img src={repository.owner.avatar_url}
+              alt={repository.owner.login} />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <div>
-            <strong>MatSa20/Eagle-React-Project</strong>
-            <p>Meu primeiro projeto em React.JS</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href='teste'>
-          <img src='https://avatars3.githubusercontent.com/u/38141771?s=460&u=671cdfd5dd82b545234d3ffeadf638bd286ddf9e&v=4'
-            alt='Matheus Sá' />
-
-          <div>
-            <strong>MatSa20/Eagle-React-Project</strong>
-            <p>Meu primeiro projeto em React.JS</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href='teste'>
-          <img src='https://avatars3.githubusercontent.com/u/38141771?s=460&u=671cdfd5dd82b545234d3ffeadf638bd286ddf9e&v=4'
-            alt='Matheus Sá' />
-
-          <div>
-            <strong>MatSa20/Eagle-React-Project</strong>
-            <p>Meu primeiro projeto em React.JS</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   )
