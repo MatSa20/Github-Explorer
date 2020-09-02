dcbd02a53f58aea27f4dfe77183e7f30c79c3064
matheus <matheus.sa.alves@outlook.com>
2020-09-02 12:13:27 -0300

    Fase 5: Criando interface dos resultados da busca por repositorios e atribuindo as informações com AXIOS

--- src/pages/Repository/index.jsx
+++ src/pages/Repository/index.jsx
@@ -1,11 +1,81 @@
-import React from 'react'
-import { useRouteMatch } from 'react-router-dom'
+/* eslint-disable no-unused-vars */
+/* eslint-disable jsx-a11y/alt-text */
+import React, { useEffect, useState } from 'react'
+import { useRouteMatch, Link } from 'react-router-dom'
+import logoImg from '../../assets/logo.svg'
+import { Header, RepositoryInfo, Issues } from './styles'
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+import api from '../../services/api'
+
 
 export default function Repository() {
 
+  const [repository, setRepository] = useState(null)
+  const [issues, setIssues] = useState([])
+
   const { params } = useRouteMatch()
 
+  useEffect(() => {
+    api.get(`repos/${params.repository}`).then(response => {
+      setRepository(response.data)
+    })
+
+    api.get(`repos/${params.repository}/issues`).then(response => {
+      setIssues(response.data)
+    })
+  }, [params.repository])
+
   return (
-    <h1>Repository: {params.repository}</h1>
+    <>
+      <Header>
+        <img src={logoImg} alr='Github Explorer' />
+        <Link to='/'>
+          <FiChevronLeft size={16} />
+        Voltar
+      </Link>
+      </Header>
+
+      {repository && (
+        <RepositoryInfo>
+          <header>
+            <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+
+            <li>
+              <strong>{repository.open_issues_count}</strong>
+              <span>Issues abertas</span>
+            </li>
+          </ul>
+        </RepositoryInfo>
+      )}
+
+      <Issues>
+        {issues.map(issue => (
+          <a key={issue.id} href={issue.html_url}>
+
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Issues>
+    </>
   )
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -7,7 +7,8 @@ import Repository from '../pages/Repository'
 export default function Routes() {
   return (
     <Switch>
-      <Route path='/' exact component={Dashboard} />
+      <Route path='/' exact component={Dashboard}>
+      </Route>
       {/* O + indica que tudo que tem depois da barra é um parametro */}
       <Route path='/repository/:repository+' component={Repository} />
     </Switch>
